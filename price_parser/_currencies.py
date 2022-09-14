1bf4cb23791637c78e20a983c74bcad9032c8900
Dashgin <76277687+dashgin@users.noreply.github.com>
2022-09-14 15:59:12 +0400

    update currency main symbol and currency native symbol for AZN(Azerbaijani Manat)

--- price_parser/_currencies.py
+++ price_parser/_currencies.py
@@ -109,9 +109,9 @@ CURRENCIES: Dict[str, Dict] = {
         "np": "Aruban Florins"
     },
     "AZN": {
-        "s": "man.",
+        "s": "₼",
         "n": "Azerbaijani Manat",
-        "sn": "ман.",
+        "sn": "₼",
         "d": 2,
         "r": 0,
         "np": "Azerbaijani manats"
