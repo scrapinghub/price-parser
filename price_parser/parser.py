9e06e1c409cf7989564384c443d6bf432066a8e2
Mohsin Surani <mohsinmax786@gmail.com>
2024-10-22 13:03:33 +0100

    Add Python 3.13 support (#66)

--- price_parser/parser.py
+++ price_parser/parser.py
@@ -53,7 +53,7 @@ class Price:
         currency = extract_currency_symbol(price, currency_hint)
         if currency is not None:
             currency = currency.strip()
-        if digit_group_separator:
+        if digit_group_separator is not None and price is not None:
             price = price.replace(digit_group_separator, '')
         amount_text = extract_price_text(price) if price is not None else None
         amount_num = (
