2c640581214fd626f593f37733dd3693b11a44c4
mohsinsurani <mohsinmax786@gmail.com>
2024-10-22 10:46:48 +0100

    Added code for python and env 3.13

--- setup.py
+++ setup.py
@@ -26,5 +26,10 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
 )
